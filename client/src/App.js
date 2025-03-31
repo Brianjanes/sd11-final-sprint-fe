f312a6a8a20188396387a12bfcd854ad068854b0
Brianjanes <janes.brian@gmail.com>
2025-03-31 10:45:36 -0230

    template for project

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import "./styles/App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <main>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
